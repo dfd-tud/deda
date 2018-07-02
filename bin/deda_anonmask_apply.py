43728e8fe5244a8b54f28979e9a5c53c9bd783ee
Timo <timo.juez@gmail.com>
2018-07-02 21:55:59 +0300

    input/output PDF format; multipage support

--- bin/deda_anonmask_apply.py
+++ bin/deda_anonmask_apply.py
@@ -13,33 +13,13 @@ the Free Software Foundation, either version 3 of the License, or
 
 import sys, argparse, json
 import numpy as np; import cv2
-from libdeda.psfile import PSFile
+from io import BytesIO
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from reportlab.pdfgen import canvas
 from libdeda.cmyk_to_rgb import BLACK, YELLOW, CYAN
 
 
 DOTRADIUS = 0.004 #in
-
-PS_PLACE_EPS = """
-/placeEPS 
-{
-    userdict begin
-
-    /__be_state     save            def
-    /__ds_count     countdictstack  def
-    /__os_count     count 5 sub     def
-    /showpage       {}              def
-
-    initgraphics
-    translate 
-    dup scale 
-    run
-
-    count           __os_count sub  { pop } repeat
-    countdictstack  __ds_count sub  { end } repeat
-                    __be_state              restore
-    end
-} bind def
-"""
 
 
 class Main(object):
@@ -52,7 +32,6 @@ class Main(object):
         parser.add_argument("--dotradius", type=float, metavar="INCHES", default=DOTRADIUS, help='Dot radius, default=%f'%DOTRADIUS)
         parser.add_argument("--black", action="store_true", default=False, help='DEBUG: Print dots black instead of yellow')
 
-
         parser.add_argument("mask", type=str, metavar="MASK", help='Path to mask by anonmask_create')
         parser.add_argument("page", type=str, metavar="PAGE", help='Path to page that shall be printed')
         parser.add_argument("-v",'--verbose', action='count', default=0, help='Fehlerausgabe')
@@ -62,45 +41,74 @@ class Main(object):
         self.argparser()
     
     def __call__(self):
-        A4 = (8.3, 11.7)
-        OUTFILE = "masked.ps"
+        OUTFILE = "masked.pdf"
 
-        dpi = 72
         with open(self.args.mask) as fp: 
             d = json.load(fp)
         proto = d["proto"]
-        hps = d["hps"]
-        vps = d["vps"]
+        self.hps = d["hps"]
+        self.vps = d["vps"]
         xOffset = self.args.xoffset or d["x_offset"]
         yOffset = self.args.yoffset or d["y_offset"]
-        proto = [(xDot+xOffset,yDot+yOffset) for xDot, yDot in proto]
+        self.proto = [(xDot+xOffset,yDot+yOffset) for xDot, yDot in proto]
         
-        dotRadius = self.args.dotradius
+        self.dotRadius = self.args.dotradius
         print("Parameters:")
         print("\tTDM x offset: \t%f"%xOffset)
         print("\tTDM y offset: \t%f"%yOffset)
-        print("\tDot radius: \t%f"%dotRadius)
+        print("\tDot radius: \t%f"%self.dotRadius)
         
-        ps = PSFile(OUTFILE, margin=0, paper="A4")
-        ps.append(PS_PLACE_EPS)
-        if self.args.black: ps.append("%d %d %d setrgbcolor"%BLACK)
-        else: ps.append("%d %d %d setrgbcolor"%YELLOW)
-        for x_ in range(int(ps.width/hps/dpi+1)):
-            for y_ in range(int(ps.height/vps/dpi+1)):
-                for xDot, yDot in proto:
-                    x = (x_*hps+xDot%hps)*dpi
-                    y = ps.height - (y_*vps+yDot%vps)*dpi
-                    if x > ps.width or y > ps.height: continue
-                    ps.append("%f %f %f 0 360 arc"%(x,y,dotRadius*dpi))
-                    ps.append("fill")
-        #ps.append("(%s) run"%self.args.page)
-        #ps.append("(%s) 1 100 100 placeEPS"%self.args.page)
-        #ps.append("BeginEPSF")
-        with open(self.args.page) as fp:
-            ps.append(fp.read())
-        #ps.append("EndEPSF")
-        ps.close()
+        pdfWatermark(self.args.page, self.createMask, OUTFILE)
         print("Document written to '%s'"%OUTFILE)
+
+    def createMask(self, w, h):
+        w = float(w)
+        h = float(h)
+        colour = BLACK if self.args.black else YELLOW
+        io = BytesIO()
+        c = canvas.Canvas(io, pagesize=(w,h) )
+        c.setStrokeColorRGB(*colour)
+        c.setFillColorRGB(*colour)
+        for x_ in range(int(w/self.hps/72+1)):
+            for y_ in range(int(h/self.vps/72+1)):
+                for xDot, yDot in self.proto:
+                    x = (x_*self.hps+xDot%self.hps)*72
+                    y = h - (y_*self.vps+yDot%self.vps)*72
+                    if x > w or y > h: continue
+                    c.circle(x,y,self.dotRadius*72,stroke=0,fill=1)
+        c.showPage()
+        c.save()
+        return io
+
+
+def pdfWatermark(infile, maskCreator, outfile):
+    """
+    For each page from infile, maskCreator will be called with its dimensions
+    and shall return a single page PDF to be put on top. The result is
+    written to outfile.
+    @infile String,
+    @maskCreator Function,
+    @outfile String
+    """
+    output = PdfFileWriter()
+    input_ = PdfFileReader(open(infile,"rb"))
+    pageBoxes = [input_.getPage(p_nr).mediaBox
+        for p_nr in range(input_.getNumPages())
+    ]
+    maxWidth = max([b.getWidth() for b in pageBoxes])
+    maxHeight = max([b.getHeight() for b in pageBoxes])
+    maskPdf = maskCreator(maxWidth, maxHeight)
+
+    for p_nr in range(input_.getNumPages()):
+        page = input_.getPage(p_nr)
+        box = page.mediaBox
+        #maskPdf = maskCreator(box.getWidth(),box.getHeight() )
+        maskPage = PdfFileReader(maskPdf).getPage(0)
+        page.mergePage(maskPage)
+        output.addPage(page)
+        
+    with open(outfile,"wb") as f:
+        output.write(f)
 
 
 main = lambda:Main()()
