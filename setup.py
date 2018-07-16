6d2cf8b74c42dfc11b4adcd5c1406991f128e69b
Timo <timo.juez@gmail.com>
2018-07-16 14:23:40 +0300

    pip bin install fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='deda',
-      version='1.0-beta2',
+      version='1.0-beta3',
       python_requires='>=3.3',
       description='tracking Dots Extraction, Decoding and Anonymisation toolkit',
       long_description=long_description,
@@ -17,13 +17,16 @@ setup(name='deda',
       install_requires=[
           'numpy', 'opencv-python', 'argparse', 'scipy', 'Pillow'
       ],
-      entry_points={'console_scripts': [
-        'deda_anonmask_apply = bin.deda_anonmask_apply:main',
-        'deda_anonmask_create = bin.deda_anonmask_create:main',
-        'deda_clean_document = bin.deda_clean_document:main',
-        'deda_compare_prints = bin.deda_compare_prints:main',
-        'deda_parse_print = bin.deda_parse_print:main',
-      ]},
+      scripts=['bin/deda_anonmask_apply', 'bin/deda_anonmask_create',
+        'bin/deda_clean_document', 'bin/deda_compare_prints',
+        'bin/deda_parse_print'],
+      #entry_points={'console_scripts': [
+      #  'deda_anonmask_apply = bin.deda_anonmask_apply:main',
+      #  'deda_anonmask_create = bin.deda_anonmask_create:main',
+      #  'deda_clean_document = bin.deda_clean_document:main',
+      #  'deda_compare_prints = bin.deda_compare_prints:main',
+      #  'deda_parse_print = bin.deda_parse_print:main',
+      #]},
       include_package_data=True,
       zip_safe=False,
       classifiers=(
