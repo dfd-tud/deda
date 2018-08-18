70b1afe65191e7b261fadfec8110c540e7a382a9
Timo <timo.juez@gmail.com>
2018-08-18 13:48:36 +0300

    minor fixes

--- libdeda/__init__.py
+++ libdeda/__init__.py
@@ -1,2 +1,7 @@
 from . import print_parser, privacy, extract_yd, pattern_handler
+from .print_parser import PrintParser
+from .extract_yd import YDX
+from .privacy import (AnonmaskApplierTdm, AnonmaskApplier, cleanScan,
+    calibrationScan2Anonmask, createCalibrationpage)
+from .pattern_handler import *
 
